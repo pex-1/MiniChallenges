9c65c88d36b7ec8e2b8d384628b3d44e81f76747
pex-1 <pecek.danijel@gmail.com>
2025-11-14 23:19:17 +0100

    multi stage painter initial

--- app/src/main/java/com/example/minichallenges/MainActivity.kt
+++ app/src/main/java/com/example/minichallenges/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.minichallenges
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.example.minichallenges.challenges.september.AccessibleAudioSchedule
+import com.example.minichallenges.challenges.september.MultiStageTimelinePainter
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         setContent {
-            AccessibleAudioSchedule()
+            MultiStageTimelinePainter()
         }
     }
 }
